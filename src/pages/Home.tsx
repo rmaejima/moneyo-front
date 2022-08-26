c131549b1148716ad1ccfe83affbc7fd279fba5d
rmaejima <ryo.maejii@gmail.com>
2022-08-26 20:10:49 +0900

    feat: show dummy user card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 import { FaHome } from 'react-icons/fa';
 
-import { PageTitle } from 'components/common/PageTitle';
+import { PageSectionTitle, PageTitle } from 'components/common/PageTitle';
 import { SafeArea } from 'components/common/SafeArea';
+import { UserCard } from 'components/home/UserCard';
+
+import { User } from 'types/user';
 
 export const Home: React.VFC = () => {
+  const mockUserData: User = {
+    userId: 'sample',
+    name: '早起きするマン',
+    experiencePoint: 23,
+    experiencePointToNextLevel: 100,
+    castlePoint: 10,
+    level: 5,
+    bedTime: new Date(),
+    wakeUpTime: new Date(),
+  };
   return (
     <SafeArea>
       <PageTitle>
         <FaHome />
         <h1>HOME</h1>
       </PageTitle>
+      <PageSectionTitle>勇者について</PageSectionTitle>
+      <UserCard user={mockUserData} />
+      <PageSectionTitle>城について</PageSectionTitle>
     </SafeArea>
   );
 };
