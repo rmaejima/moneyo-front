86d963d8747b02f45d5a3f8d284e399d044f0f9d
rmaejima <ryo.maejii@gmail.com>
2022-08-26 20:10:13 +0900

    feat: add section title

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -6,13 +6,20 @@ export const PageTitle = styled.div`
 
   margin-bottom: 2rem;
   color: ${(p) => p.theme.colors.text.base};
-  font-size: 1.5rem;
+  font-size: 2rem;
   font-weight: bold;
 
   > :not(:first-child) {
     margin-left: 1rem;
     color: ${(p) => p.theme.colors.text.base};
-    font-size: 1.5rem;
+    font-size: 2rem;
     font-weight: bold;
   }
+`;
+
+export const PageSectionTitle = styled.div`
+  margin-bottom: 1.5rem;
+  color: ${(p) => p.theme.colors.text.base};
+  font-size: 1.5rem;
+  font-weight: bold;
 `;
