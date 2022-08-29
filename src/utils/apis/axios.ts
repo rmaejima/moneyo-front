3bf38642a252698ccaa612deb0a4cf787b76e092
rmaejima <ryo.maejii@gmail.com>
2022-08-29 14:50:50 +0900

    fix: api base url

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 'http://localhost:8080';
 
 const setOptions = async (
   options: AxiosRequestConfig,
