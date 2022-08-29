685662a48ae413946f0d417613fc84f3fd4c4543
rmaejima <ryo.maejii@gmail.com>
2022-08-29 11:04:55 +0900

    feat: add bedin button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { FaHome } from 'react-icons/fa';
+import styled from 'styled-components';
 
+import { Button } from 'components/common/Button';
 import { PageSectionTitle, PageTitle } from 'components/common/PageTitle';
 import { SafeArea } from 'components/common/SafeArea';
 import { UserCard } from 'components/home/UserCard';
@@ -21,13 +23,38 @@ export const Home: React.VFC = () => {
   };
   return (
     <SafeArea>
-      <PageTitle>
-        <FaHome />
-        <h1>HOME</h1>
-      </PageTitle>
-      <PageSectionTitle>勇者について</PageSectionTitle>
-      <UserCard user={mockUserData} />
-      <PageSectionTitle>城について</PageSectionTitle>
+      <TitleSectionContainer>
+        <StyledPageTitle>
+          <FaHome />
+          <h1>HOME</h1>
+        </StyledPageTitle>
+        {/* TODO:値によって制御 */}
+        <Button>ベッドに入る</Button>
+      </TitleSectionContainer>
+      <SectionContainer>
+        <PageSectionTitle>勇者について</PageSectionTitle>
+        <UserCard user={mockUserData} />
+      </SectionContainer>
+      <SectionContainer>
+        <PageSectionTitle>城について</PageSectionTitle>
+        <p>現在開発中です</p>
+      </SectionContainer>
     </SafeArea>
   );
 };
+
+const StyledPageTitle = styled(PageTitle)`
+  margin-bottom: 0;
+`;
+
+const TitleSectionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 2rem;
+`;
+
+const SectionContainer = styled.div`
+  margin-bottom: 3rem;
+`;
